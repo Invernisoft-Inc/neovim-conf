8bae1b3a47834920b87226785795cc6bd95794ac
Thanatermesis <thanatermesis@gmail.com>
2023-04-16 20:31:07 -0500

    added installer templates

---
+++ install-elivim.sh
@@ -0,0 +1,195 @@
+#!/bin/bash
+SOURCE="install-elivim.sh"
+source /usr/lib/elive-tools/functions 1>/dev/null 2>&1 || true
+set -x
+
+# NEVER set -e, so traps are ignored!
+#set -e
+# ERR is catched too an inherited in parents, needed
+set -E
+# catch signals
+set -o functrace
+shopt -s extdebug
+# avoid ! expansion
+set +o histexpand
+# verbose:
+#set -x
+sources="$(pwd)"
+
+# elive functions pre {{{
+if [[ -n "$NOCOLOR" ]] || ! ((is_interactive)) ; then
+    el_c_n=""
+    el_c_r=""
+    el_c_r2=""
+    el_c_g=""
+    el_c_g2=""
+    el_c_y=""
+    el_c_y2=""
+    el_c_m=""
+    el_c_m2=""
+    el_c_c=""
+    el_c_c2=""
+    el_c_b=""
+    el_c_b2=""
+    el_c_gr=""
+    el_c_gr2=""
+    el_c_w=""
+    el_c_w2=""
+
+    el_c_blink=""
+    el_c_underline=""
+    el_c_italic=""
+    el_c_bold=""
+else
+    el_c_gr="\033[1;30m" # Gray color
+    el_c_gr2="\033[0;30m" # Gray2 color
+    el_c_r="\033[1;31m" # Red color  (orig: red)
+    el_c_r2="\033[0;31m" # Red2 color  (orig: red)
+    el_c_g="\033[1;32m" # Green color  (orig: green)
+    el_c_g2="\033[0;32m" # Green2 color  (orig. green2)
+    el_c_y="\033[1;33m" # Yellow color  (orig. yellow)
+    el_c_y2="\033[0;33m" # Yellow2 color  (orig. yellow)
+    el_c_b="\033[1;34m" # Blue color
+    el_c_b2="\033[0;34m" # Blue2 color
+    el_c_m="\033[1;35m" # Magenta color
+    el_c_m2="\033[0;35m" # Magenta2 color
+    el_c_c="\033[1;36m" # Cyan color
+    el_c_c2="\033[0;36m" # Cyan2 color
+    el_c_w="\033[1;37m" # White
+    el_c_w2="\033[0;37m" # White strong
+    el_c_n="\033[0;39m" # Normal color  (orig: normal)
+
+    if ((is_console)) ; then
+        ## gray's are not visible in console when vga=normal (t460s doesnt configure the good one so it fallbacks to normal), use cyan's instead
+        el_c_gr="\033[1;36m" # Gray color
+        #el_c_gr2="\033[0;36m" # Gray2 color
+    fi
+    #else
+        #el_c_gr="\033[1;30m" # Gray color
+        #el_c_gr2="\033[0;30m" # Gray2 color
+    #fi
+
+    el_c_blink="\033[5m" # Blink 'color' effect  (orig. blink)
+    el_c_underline="\033[4m" # Underline 'color' effect  (orig. underline)
+    el_c_italic="\033[3m" # Italic 'color' effect
+    el_c_bold="\033[1m" # Bold 'color' effect
+fi
+
+el_debug(){
+    if [[ "$EL_DEBUG" -ge 2 ]] ; then
+        echo -e "${el_c_c}D: ${el_c_c}${@}${el_c_n}" 1>&2
+    fi
+}
+el_info(){
+    echo -e "${el_c_c2}I: ${el_c_c2}${@}${el_c_n}" 1>&2
+}
+el_warning(){
+    echo -e "${el_c_y2}W: ${el_c_y2}${@}${el_c_n}" 1>&2
+}
+el_error(){
+    echo -e "${el_c_r}E: ${el_c_r}${@}${el_c_n}" 1>&2
+}
+
+#===  FUNCTION  ================================================================
+#          NAME:  el_confirm
+#   DESCRIPTION:  ask (console) for confirmation
+#    PARAMETERS:  question
+#       RETURNS:  true | false
+#===============================================================================
+el_confirm(){
+    # pre {{{
+    local reply question flag_glob
+
+    question="$1"
+
+    # }}}
+    # return answer {{{
+
+    echo -en "\n$question [y/n]: "
+    if [[ -n "$ZSH_VERSION" ]] ; then
+        read reply
+    else
+        read -e reply
+    fi
+
+    case "$reply" in
+        y|Y|Yes|YES|s|S)
+            return  0
+            ;;
+        n|N|no|NO|nope)
+            return  1
+            ;;
+        *)
+            # repeat question until confirmation
+            if el_confirm "${el_c_b2}${@}${el_c_n}" ; then
+                return 0
+            else
+                return 1
+            fi
+            ;;
+    esac
+
+    # }}}
+}
+
+
+#
+# Get a more complete elive-functions
+#
+if [[ -s "/usr/lib/elive-tools/functions" ]] ; then
+    source /usr/lib/elive-tools/functions 1>/dev/null 2>&1 || true
+else
+    # local copy
+    if [[ -s "elive-functions" ]] ; then
+        source elive-functions 1>/dev/null 2>&1 || true
+    fi
+fi
+
+# - elive functions pre }}}
+# traps {{{
+
+exit_error(){
+    set +x
+
+    # mark a failed step
+    echo $$ > "/tmp/.${SOURCE}.failed"
+
+    exit 1
+}
+exit_ok(){
+    if [[ "$?" = 0 ]] ; then
+        echo -e "${el_c_g}${el_c_blink}Help Elive to continue making amazing things with a grateful donation! - https://www.elivecd.org/donate/${el_c_n}"
+    fi
+}
+trap "exit_error" ERR
+trap "exit_ok" EXIT
+#trap "exit_error" 1 2 3 6 9 11 13 14 15
+
+# - traps }}}
+# checks {{{
+if ! [[ -d "${sources}/.git" ]] ; then
+    el_error "You need to run this command from the cloned git sources directory, aborting..."
+    exit 1
+fi
+
+# }}}
+
+#
+# Start the installer
+#
+
+main(){
+    # pre {{{
+    local var
+
+    # }}}
+
+}
+
+#
+#  MAIN
+#
+main "$@"
+
+# vim: set foldmethod=marker :
+
